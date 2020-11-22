7ab4a74cfa6e76e962ffb2527136cf16d03e3bf0
Rajkumar Wadiwala <71130660+Rajkumar-27@users.noreply.github.com>
2020-11-22 20:11:32 +0530

    Update sample_config.py

--- thebot/sample_config.py
+++ thebot/sample_config.py
@@ -1,9 +1,9 @@
 class Config(object):
    LOGGER = True
-   API_ID = 69
-   API_HASH = "owo"
-   TOKEN = "uwu"
-   DB_URI = ""
+   API_ID = 1645928
+   API_HASH = "6874b8b3d4aef43cd79cc32d0fc86af8"
+   TOKEN = "1385832631:AAGccX7ZIYKnwXd3nJxi-WXobtd-4iaUkKc"
+   DB_URI = "postgres://mrbcrvit:m3juoeaXEgJMOeUXhLqikEQaIA1vmfpb@satao.db.elephantsql.com:5432/mrbcrvit"
 
 class Development(Config):
    TEST_DEVELOP = True
